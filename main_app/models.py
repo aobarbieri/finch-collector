57533cfe5d98d0e601547a007a7b86b335888195
aobarbieri <aobarbieri@gmail.com>
2024-01-10 12:42:36 -0500

    Create Finch model, create objects in the DB and render on the page, register the Finch model with the admin portal

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=50)
+    populationTrend = models.CharField(max_length=50)
+    note = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
-finches = [
-    {'name': 'Black Rosy-Finch', 'populationTrend': 'Decreasing', 'note': 'The Black Rosy-Finches nest in secluded crevasses along cliffs in alpine areas.'},
-    {'name': 'Common Redpoll', 'populationTrend': 'Unknown', 'note': 'Common Redpolls sometimes escape the cold of winter nights by burrowing into snow. (To keep redpolls and other birds safe at feeders, it is recommended that you clean your feeders with a diluted bleach solution several times a week, and make sure feeders are dry before filling them with seed. This helps prevent salmonella and other infections.)'},
-    {'name': 'Cassia Crossbill', 'populationTrend': 'Decreasing', 'note': 'The Cassia Crossbill was once considered to be one of ten types of Red Crossbill. However, by 2017, careful study revealed that it doesn\'t migrate or breed with other crossbills. Its name comes from Cassia County, Idaho.'}
-]
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -14,6 +10,7 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
